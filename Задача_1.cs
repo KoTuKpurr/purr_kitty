98af60324c72b353cc0c9c4c7eb9ee865a4581b2
Milena <milshegoleva@gmail.com>
2020-11-24 12:26:37 +0300

    Первые три задачи

---
+++ Задача_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            double octatok = number % 2;
+            if (octatok == 0)
+                Console.WriteLine("Число четное");
+            else
+                Console.WriteLine("Число нечетное");
+
+        }
+    }
+}
---
+++ Задача_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int odin = 1;
+            Console.Write("{0} ", odin);
+            int dva = 1;
+            Console.Write("{0} ", dva);
+            int sum = 0;
+            while (number >= sum)
+            {
+                sum = odin + dva;
+                if (number >= sum)
+                    Console.Write("{0} ", sum);
+
+                odin = dva;
+                dva = sum;
+            }
+        }
+    }
+}
